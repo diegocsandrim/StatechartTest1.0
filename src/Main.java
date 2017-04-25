e0692353b22742217f6e8de6078dfa359d60ed0e
Vinicius Lima <viniciuslima@usp.br>
2017-04-24 23:30:07 -0300

    alterações nos métodos de exportação: mudança para JFileChooser e filtro de extensão; alertas sobre critérios de cobertura não implementados; ajustes de tamanho de fontes; inicializar janela principal centralizada na tela; exibir erro ao tentar gerar casos de testes sem selecionar um arquivo .sct (botão "Create Test Cases"); correção na exportação CSV (remoção das quebras de linhas); filtro de extensão ao selecionar arquivo .sct; limpar resultados da tabela ao gerar novos casos de testes (ao clicar no botão "Create Test Cases").

---
+++ src/Main.java
@@ -0,0 +1,289 @@
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Set;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+
+import xml.handler.XMLProcessor;
+import xml.statechart.Statechart;
+
+public class Main {
+
+	public enum Criteria{
+		ALL_PATHS("all paths"),
+		ALL_STATES("all states"),
+		ALL_TRANSITIONS("all transitions");
+		
+		
+		private final String text;
+		private Criteria(final String text) {
+			this.text = text;
+		}
+		public String getText() {
+			return this.text;
+		}
+	    public String toString() {
+	        return text;
+	    }
+	}
+	
+	private JFrame frame;
+	private JTextField selectedFile;
+	private Criteria coverage;
+	JButton btnNewButton;
+	
+	public static Output out;
+	public  JTable table;
+	public DefaultTableModel model; 
+
+	private Set<String> testPaths;
+	private Set<String> csvLines;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	//action for the create test cases button
+	private void actionCreateTestCases_AllTransitions() {				
+		XMLProcessor xml = new XMLProcessor();
+		String filePath = selectedFile.getText();
+		xml.createXmlFromYakindu(filePath);
+		
+		try {
+			long startTime = System.nanoTime();
+			Statechart statechart = xml.createStatechartFromXml("temp.xml");
+			statechart.constructStateIdHash();
+			TestGenerator tg = new TestGenerator(statechart);
+			testPaths = tg.createTestPaths();
+			csvLines = tg.csvLines;
+			long endTime = System.nanoTime();
+			long duration = (endTime - startTime)/1000;
+			System.out.println("Test Cases Generation takes: " + duration + " miliseconds");
+		} catch(Exception ex) {
+			JOptionPane.showMessageDialog(null, "An error occurred while generating test cases. "
+					+ "Check for errors in your Yakindu SCT statechart file or maybe we can't help you. Sorry :(", "Error", JOptionPane.ERROR_MESSAGE);
+			ex.printStackTrace();
+		}
+	}
+	
+	//action for the open button
+	private void actionOpenStatechartButton() {
+		String filePath = "";
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		fileChooser.setMultiSelectionEnabled(false);
+		fileChooser.setFileFilter(new FileNameExtensionFilter(".sct", "sct"));
+		int result = fileChooser.showOpenDialog(frame);
+		if (result == JFileChooser.APPROVE_OPTION) {
+		    File selectedFile = fileChooser.getSelectedFile();
+		    filePath = selectedFile.getAbsolutePath();
+		}
+		selectedFile.setText(filePath);
+
+	}
+	
+	//action for the export spmf button
+	private void actionExportSPMFButton() {        
+		String spmfPath = this.getSaveLocation(null);
+		if (spmfPath != null) {        	 
+        	TestPathsAdapter adapter = new TestPathsAdapter();
+            Set<String> adaptedPaths = adapter.adaptToSMPF(testPaths);
+            out.writeSPMFToFile(spmfPath, adaptedPaths);
+        }
+	}
+	
+	//action for the export csv button
+	private void actionExportCsvButton() {		       
+        String csvPath = this.getSaveLocation("csv");
+		if (csvPath != null) {        	 
+			out.writeCsvToFile(csvPath, csvLines);
+        }               
+	}
+	
+	private String getSaveLocation(String extension) {
+	 	if(table.getRowCount() == 0) {
+        	JOptionPane.showMessageDialog(null, "No data to export!", "Error", JOptionPane.ERROR_MESSAGE);
+        } else {		
+			JFileChooser fileChooser = new JFileChooser();
+	        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			fileChooser.setAcceptAllFileFilterUsed(false);
+			fileChooser.setMultiSelectionEnabled(false);
+			
+			if(extension != null)
+				fileChooser.setFileFilter(new FileNameExtensionFilter("."+extension, extension));
+	        
+	        int option = fileChooser.showSaveDialog(null);
+	        if(option == JFileChooser.APPROVE_OPTION)
+	        	return fileChooser.getCurrentDirectory().toString() + System.getProperty("file.separator") + fileChooser.getSelectedFile().getName();
+        }
+	 	return null;
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 984, 596);
+		frame.setTitle("Statechart Test Tool");
+		frame.setIconImage(new ImageIcon("Statechart_Test_Tool.png").getImage());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(16, 80, 962, 450);
+		frame.getContentPane().add(scrollPane);
+		
+
+		String[] columnNames = {"State","Transition","Test Path","Expected State"};
+		model= new DefaultTableModel(null,columnNames);
+
+		table = new JTable(model);
+
+		scrollPane.setViewportView(table);
+		table.setFont(new Font("Arial", Font.PLAIN, 10));
+		table.setBounds(68, 100, 1, 1);
+		
+		out = new Output(table);
+		
+		btnNewButton = new JButton("Create test cases");
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 10));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(selectedFile.getText() == null || selectedFile.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Please open your Yakindu Statechart file!", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				switch (coverage){
+					case ALL_PATHS:
+							//System.out.println("Creating Test Cases for All Paths Coverage Criteria");
+							JOptionPane.showMessageDialog(null, "This coverage criteria has not been implemented yet!", "Not Implemented", JOptionPane.ERROR_MESSAGE);
+							break;
+					case ALL_STATES:
+							//System.out.println("Creating Test Cases for All States Coverage Criteria");
+							JOptionPane.showMessageDialog(null, "This coverage criteria has not been implemented yet!", "Not Implemented", JOptionPane.ERROR_MESSAGE);
+							break;
+					case ALL_TRANSITIONS:
+							System.out.println("Creating Test Cases for All Transitions Coverage Criteria");
+							actionCreateTestCases_AllTransitions();
+							break;	
+				}
+			}
+		});
+		btnNewButton.setBounds(844, 39, 124, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		selectedFile = new JTextField();
+		selectedFile.setFont(new Font("Arial", Font.PLAIN, 10));
+		selectedFile.setBounds(264, 4, 579, 28);
+		frame.getContentPane().add(selectedFile);
+		selectedFile.setColumns(10);			
+		
+		JLabel lblNewLabel = new JLabel("Path to your Yakindu Statechart (.sct):");
+		lblNewLabel.setBounds(16, 0, 258, 37);
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+		frame.getContentPane().add(lblNewLabel);
+
+		JLabel lblCriteria = new JLabel("Test Criteria:");
+		lblCriteria.setBounds(16, 30, 258, 37);
+		lblCriteria.setFont(new Font("Arial", Font.PLAIN, 12));
+		frame.getContentPane().add(lblCriteria);
+
+		String[] criteriaList = { Criteria.ALL_PATHS.getText(), Criteria.ALL_TRANSITIONS.getText(), Criteria.ALL_STATES.getText()};
+		JComboBox<String> testCriteriaList = new JComboBox<>(criteriaList);
+		testCriteriaList.setFont(new Font("Arial", Font.PLAIN, 12));
+
+		testCriteriaList.setBounds(264, 37, 258, 27);
+		testCriteriaList.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				String criteria=(String)testCriteriaList.getSelectedItem();
+				if (criteria.compareToIgnoreCase(Criteria.ALL_PATHS.getText())==0){
+					Main.this.coverage=Criteria.ALL_PATHS;
+				}
+				if (criteria.compareToIgnoreCase(Criteria.ALL_STATES.getText())==0){
+					Main.this.coverage=Criteria.ALL_STATES;
+				} 
+				if (criteria.compareToIgnoreCase(Criteria.ALL_TRANSITIONS.getText())==0){
+					Main.this.coverage=Criteria.ALL_TRANSITIONS;
+				} 
+			}			
+		});
+		testCriteriaList.setSelectedIndex(1);
+		frame.getContentPane().add(testCriteriaList);
+		
+		JButton btnOpenStatechart = new JButton("Open Statechart");
+		btnOpenStatechart.setFont(new Font("Arial", Font.PLAIN, 10));
+		
+		btnOpenStatechart.setBounds(844, 5, 124, 29);
+		btnOpenStatechart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionOpenStatechartButton();
+			}
+		});
+		frame.getContentPane().add(btnOpenStatechart);
+		
+		JButton btnExportInSpmf = new JButton("Export in SPMF format");
+		btnExportInSpmf.setFont(new Font("Arial", Font.PLAIN, 10));
+		btnExportInSpmf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionExportSPMFButton();
+			}
+		});
+		btnExportInSpmf.setBounds(660, 532, 163, 29);
+		frame.getContentPane().add(btnExportInSpmf);
+		
+		JButton btnExportTocsv = new JButton("Export to .csv");
+		btnExportTocsv.setFont(new Font("Arial", Font.PLAIN, 10));
+		btnExportTocsv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionExportCsvButton();
+			}
+		});
+		btnExportTocsv.setBounds(844, 532, 124, 29);
+		frame.getContentPane().add(btnExportTocsv);
+		
+		
+	}
+}
---
+++ src/Output.java
@@ -0,0 +1,84 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Set;
+
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.table.DefaultTableModel;
+
+
+public class Output {
+
+	private JTextArea textArea;
+	
+	//The table to which we are printing the output
+	private JTable table;
+
+	Output(JTextArea ta) {
+		textArea = ta;
+	}
+	
+	Output(JTable table) {
+		this.table = table;
+	}
+	
+	public void println(String str) {
+		textArea.append(str+"\n");
+	}
+	
+	public void clear() {
+		textArea.setText(null);
+	}
+	
+	public void cleanTable() {
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		while( model.getRowCount() > 0 ){
+	        model.removeRow(0);
+	    }
+	}
+	
+	//Prints a row in the table
+	public void printRow(String state, String transition, String path, String expected) {
+		DefaultTableModel myModel = (DefaultTableModel) table.getModel();
+		myModel.addRow(new Object[]{state, transition, path, expected});
+	}
+	
+	//write to the file used by SPMF
+	public void writeSPMFToFile(String filePath, Set<String> lines) {
+		try
+		{
+		    FileWriter writer = new FileWriter(filePath);
+			for (String line : lines) {
+				writer.append(line+"\n");
+			}
+				
+		    writer.flush();
+		    writer.close();
+		}
+		catch(IOException e)
+		{
+		     e.printStackTrace();
+		}
+	}
+	
+	//write to csv file
+	public void writeCsvToFile(String filePath, Set<String> csvContent) {
+		System.out.println("=======" + filePath);
+		try
+		{
+		    FileWriter writer = new FileWriter(filePath+".csv");
+		    writer.append("State,Transition,Test Path,Expected State\n");
+			for (String line : csvContent) {
+				System.out.println(line.replace(System.getProperty("line.separator"), ""));
+				writer.append(line+"\n");
+			}
+				
+		    writer.flush();
+		    writer.close();
+		}
+		catch(IOException e)
+		{
+		     e.printStackTrace();
+		}
+	}
+}
